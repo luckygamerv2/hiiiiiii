66a4cfca6ee85bb674c7df6039f36e9ff902f482
luckygamerv2 <45069336+luckygamerv2@users.noreply.github.com>
2019-03-14 21:28:14 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');const client = new Discord.Client(); client.on('ready', () => { console.log(`Logged in as ${client.user.tag}!`);client.user.setGame(`+BkQGJw +BioGMwYqBkUGOQ +BkQGRwZF ,+BkEGRwZF +BkUGOgZKBjEGSAZG +BkUGRgZD`,"http://twitch.tv/S-F") console.log('') console.log('') console.log('+JVQ[+JVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUA]+JVc') console.log(`[Start] ${new Date()}`); console.log('+JVo[+JVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUA]+JV0') console.log('') console.log('+JVQ[+JVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQ]+JVc'); console.log(`Logged in as * [ " ${client.user.username} " ]`); console.log('') console.log('Informations :') console.log('') console.log(`servers! [ " ${client.guilds.size} " ]`); console.log(`Users! [ " ${client.users.size} " ]`); console.log(`channels! [ " ${client.channels.size} " ]`); console.log('+JVo[+JVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQ]+JV0') console.log('') console.log('+JVQ[+JVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQ]+JVc') console.log(' Bot Is Online') console.log('+JVo[+JVAlUCVQJVAlUCVQJVAlUCVQJVAlUCVQ]+JV0') console.log('') console.log('')}); 
+
+
+
+
+
+
+
+client.login('
+NTA5NzQyMDQ2MDc3MjU1NzQz.Ds7khQ.tnjDP-fX9zah6Y-duGFR46Iz09I');
