0906e773c5bc5af08819a761406a461badd23d22
luckygamerv2 <45069336+luckygamerv2@users.noreply.github.com>
2019-03-18 03:29:20 +0300

    Delete bot.js

--- bot.js
+++
@@ -1,7 +0,0 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-client.on('ready', () => {
-  client.user.setGame(`Farouk ,The Best`,'https://www.twitch.tv/lspamer');
-  console.log('BOT ONLINE');
-});
-client.login('NTA5NzQyMDQ2MDc3MjU1NzQz.Ds7khQ.tnjDP-fX9zah6Y-duGFR46Iz09I');
